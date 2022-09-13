c542f0eba8b02f27e0bd490f06eeb2a8196aba8c
JSDEV1999 <rameshlearningadvance@gmail.com>
2022-09-13 16:08:44 +0530

    updated for crud method

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -4,12 +4,25 @@ from flask import request
 
 obj = user_model()
 
-# routes
+# -------/ routes /--------
+# -------/ get all users route /-------
 @app.route('/user/getall')
 def user_getall_controller():
     return obj.user_getall_model()
-
+# -------/ get add users route /-------
 @app.route('/user/addone', methods=['POST'])
 def user_addone_controller():
     print(request.json)
     return obj.user_addone_model(request.json)
+
+# -------/ get update user route /-------
+@app.route('/user/update', methods=['PUT'])
+def user_update_controller():
+    print(request.json)
+    return obj.user_update_model(request.json)
+
+# -------/ get delete user route /-------
+@app.route('/user/delete/<id>', methods=['DELETE'])
+def user_delete_controller(id):
+    print(id)
+    return obj.user_delete_model(id)
--- models/user_model.py
+++ models/user_model.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import json
+from flask import make_response, jsonify
+
 class user_model:
     def __init__(self):
          # connection code
@@ -16,17 +18,35 @@ class user_model:
         self.cur.execute('SELECT * FROM users')
         results = self.cur.fetchall()
         if(len(results) > 0):
-            return json.dumps(results)
+            return {"data": results,
+                    "status": 200
+                    }
         else:
             return "Not Found"
+        
     def user_addone_model(self, data):
        # INSERT INTO `flask_crud`.`users` (`name`, `email`, `phone`, `role`, `password`) VALUES ('ramesh', 'ramesh@gmail.com', '7337022673', '1', 'Captain@7337');
-        print(data)
+        #print(data)
         try:
             self.cur.execute(f"INSERT INTO users(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')")
             
             return "user created"
         except TypeError:
             print("TypeError: Check list of indices")
-        
+    def user_update_model(self, data):
+        try:
+            self.cur.execute(f"UPDATE users SET name='{data['name']}', email='{data['email']}', phone='{data['phone']}' WHERE id={data['id']}")
+            if self.cur.rowcount>0:
+                return make_response({"message":"UPDATED_SUCCESSFULLY"},201)
+            else:
+                return make_response({"message":"NOTHING_TO_UPDATE"},204)
+        except TypeError:
+            print("TypeError: Check list of indices")
+    
+    def user_delete_model(self,id):
+        self.cur.execute(f"DELETE FROM users WHERE id={id}")
+        if self.cur.rowcount>0:
+            return make_response({"message":"DELETED_SUCCESSFULLY"},202)
+        else:
+            return make_response({"message":"CONTACT_DEVELOPER"},500)
         
